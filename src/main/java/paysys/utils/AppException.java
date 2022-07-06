cebd3629d7bfa3af0c0ef462cf61528e6bc9bcfc
hamyo <zabey_gvozd@list.ru>
2022-07-06 10:54:59 +0300

    Update AppException.java

--- src/main/java/paysys/utils/AppException.java
+++ src/main/java/paysys/utils/AppException.java
@@ -13,4 +13,9 @@ public class AppException extends RuntimeException {
     public AppException(String message) {
         super(message);
     }
+    
+    @Override
+    public Throwable fillInStackTrace() {
+        return this;
+    }
 }
